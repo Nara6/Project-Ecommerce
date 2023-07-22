53635f690fecd3186437eeca85c8ab95a41d7c5b
YIN Soknara <yinnara6@gmail.com>
2023-07-22 21:24:01 +0700

    config

--- web/vite.config.js
+++ web/vite.config.js
@@ -15,11 +15,16 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:1027",
+        target: "http://35.186.150.74:1026",
         changeOrigin: true,
         secure: false,
       },
     },
   },
+  pwa: {
+    workboxOptions: {
+      exclude: [/_redirects/]
+    }
+  }
 
 })
