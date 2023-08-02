c40ee327aca7f3b55fabe5c986633266d82999f1
YIN Soknara <yinnara6@gmail.com>
2023-08-03 01:44:13 +0700

    Add Admin

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 8081,
-    // host:true,
+    host:true,
     proxy: {
       "^/api": {
         // target: "http://127.0.0.1:8000",
